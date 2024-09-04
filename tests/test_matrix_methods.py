069c31c591b09aaecfbad1312f02fe2dc6172937
Egor Gurtyakin <yoda_2005@mail.ru>
2024-09-04 20:25:46 +0700

    Add dockstring to test_sum_matrices test
    Blya i forgor

--- tests/test_matrix_methods.py
+++ tests/test_matrix_methods.py
@@ -3,6 +3,7 @@ from tests.conftest import first_second_sum_expected_namedtuple
 
 
 def test_sum_matrices(use_first_second_sum_expected_matrices_dataset: first_second_sum_expected_namedtuple):
+    """Test the sum of two matrices."""
     assert (use_first_second_sum_expected_matrices_dataset.first +
             use_first_second_sum_expected_matrices_dataset.second ==
             use_first_second_sum_expected_matrices_dataset.expected)
